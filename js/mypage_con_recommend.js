e6153199c8bbc81c08cc79065b563a26f096a669
minggwen <tlsalsrud252@naver.com>
2023-08-11 13:00:53 +0900

    FeadL Add JS - con_recommend&sell_enrollitem&sell_checkOrderlist
    상품 추천 등록, 판매 상품 리스트, 상품 등록 페이지
    JS 작성 완료
    (통신에 해당하는 부분은 주석처리)

--- js/mypage_con_recommend.js
+++ js/mypage_con_recommend.js
@@ -1,28 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
     var itemImages = document.querySelectorAll(".item-image");
-    var selectedItemNames = [];
+    var selectedItem = [];
 
     itemImages.forEach(function(itemImage) {
         itemImage.addEventListener("click", function() {
             var parentBox = itemImage.closest(".recommend-item-box");
             var itemName = itemImage.getAttribute("data-item-name");
 
-            if (selectedItemNames.includes(itemName)) {
-                selectedItemNames = selectedItemNames.filter(name => name !== itemName);
+            if (selectedItem.includes(itemName)) {
+                selectedItem = selectedItem.filter(name => name !== itemName);
                 parentBox.style.border = "none"; 
             } else {
-                selectedItemNames.push(itemName);
+                selectedItem.push(itemName);
                 parentBox.style.border = "2px solid gray";
             }
 
-            console.log("Selected Items:", selectedItemNames);
+            console.log("선택한 아이템들:", selectedItem);
         });
     });
     var submitButton = document.getElementById("submit");
     submitButton.addEventListener("click", function() {
         // 서버로 보내는 로직 추가
-        console.log("Sending selected items to server:", selectedItemNames);
+        sendList(selectedItem);
     });
+    
 });
 
-
+// function sendList(selectedItem){
+//     const formData = newFormData();
+//     formData.append(
+//         new Blob([Json.stringify(selectedItem)],{
+//             type:'application/json'
+//         })
+//     );
+//     const url = "http://127.0.0.1:8000";
+//     const tokenn = localStorage.getItem('token');
+//     var myHeaders = new Headers();
+//     myHeaders.append('Authorization','Bearer'+token);
+//     fetch(url{
+//         headers: myHeaders,
+//         body:formData,
+//         method: "POST"
+//     })
+//     .then((Response)=>Response.json())
+//     .then((result)=>console.log(result))
+//     .catch((error)=>{
+//         console.error(error);
+//     })
+//     console.log("전송완료 : ",selectedItem)
+// }
