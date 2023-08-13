392813c781ed0ab8163e15a4283af164ddb27a90
jiwonsudo <wondev112@gmail.com>
2023-08-13 17:49:25 +0900

    Feat: Add checkUserRole, logOut function and fix

--- js/common/jwt_token_check.js
+++ js/common/jwt_token_check.js
@@ -1,18 +1,26 @@
-let accessToken = "access_token"; // 초기 액세스 토큰
-const refreshToken = "refresh_token"; // 리프레시 토큰
-
 /** 
- * 페이지 이동 시 초기 토큰 확인 함수
+ * (페이지 이동 시) 초기 토큰 확인 함수
  * 
  * 로컬 스토리지에 토큰이 없으면 로그인 페이지로 이동합니다.
  * valid 여부는 확인하지 않습니다.
  * 
- * @return none
+ * @returns {boolean} true: 토큰이 존재함
+ * @returns {boolean} false: 토큰이 존재하지 않음
  */
-function checkToken() {
-  if (localStorage.getItem("access_token") === null) {
-    window.location.href = "../login";
-  }
+function checkTokenExistence() {
+  return localStorage.getItem("access_token") !== null ? true : false;
+}
+
+/**
+ * 유저 역할 구별(검사) 함수
+ * 
+ * localStorage의 유저 역할을 확인하고, 반환합니다.
+ * 
+ * @returns {string} seller: 판매자
+ * @returns {string} customer: 구매자
+ */
+function checkUserRole() {
+  return localStorage.getItem("user_role");
 }
 
 /** 
@@ -29,18 +37,27 @@ function checkTokenValid(response) {
       method: 'POST',
     })
     .then(response => response.json())
-    .then(data => {
-      localStorage.setItem("access_token", data.accessToken);
+    .then(response => {
+      localStorage.setItem("access_token", response.data.accessToken);
     });
   }
 }
 
 /**
- * 로그아웃 함수 (추후 구현 예정)
+ * 로그아웃 함수
+ * 
+ * 로컬 스토리지의 토큰을 삭제하고, 메인 페이지로 이동합니다.
  */
+function logOut() {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("user_role");
+  window.alert("로그아웃 되었습니다.");
+  window.location.href = "./main-home1.html";
+}
 
 
-// 예시 - 장바구니 조회 함수
+
+// !!! 예시 - 장바구니 조회 함수
 function inquireCart() {
   fetch("https://api.example.com/cart", {
     method: "GET",
@@ -50,11 +67,11 @@ function inquireCart() {
   })
   .then(checkTokenValid(response))
   .then(response => response.json())
-  .then(data => {
-    console.log(data); // 가져온 데이터 처리
+  .then(response => {
+    console.log(response.data); // 가져온 데이터 처리
   });
 }
 
 
-
-export default {checkToken, checkTokenValid};
+// 아래 세 함수만 필요에 따라 타 파일에서 import 후 사용하면 됩니다.
+export default {checkTokenExistence, checkTokenValid, checkUserRole, logOut};
