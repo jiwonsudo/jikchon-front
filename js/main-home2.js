f76493ed82564392871c260574b4403c7090a0d6
jeong617 <loodol99@naver.com>
2023-08-15 01:47:04 +0900

    feat: mainhome1, 추천카테고리, 제품 상세페이지, 상품관리 완료

--- js/main-home2.js
+++ js/main-home2.js
@@ -1,7 +1,7 @@
 import { checkTokenValid, checkTokenExistence } from './common/jwt_token_check.js';
 
 let fetchData = [];
-let pageNum  = 0;
+let pageNum = 0;
 
 /* Header 설정 */
 var myHeaders = new Headers();
@@ -10,26 +10,61 @@ myHeaders.append('Content-Type', 'application/json');
 const urlParams = new URLSearchParams(window.location.search);
 const categoryId = urlParams.get('id');
 console.log(categoryId);
-const url = `/products?category=${categoryId}&page=${pageNum}`
+let url = `/products?category=${categoryId}&page=${pageNum}`
 
 function loadProdData() {
-    fetch(url, {
-        headers: myHeaders,
+  fetch(url, {
+    headers: myHeaders,
+    method: 'GET'
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let data1 = data.data;
+      console.log(data1);
+      fetchData = data1;
     })
-        .then((response) => response.json())
-        .then((data) => {
-            let data1 = data.data;
-            console.log(data1);
-            fetchdata = data1;
-        })
-        .catch((error) => {
-            console.error('An error occurred while loading store data:', error);
-        });
+    .catch((error) => {
+      console.error('An error occurred while loading store data:', error);
+    });
+}
+
+function loadMoreItems() {
+  pageNum++;
+  let nextPageData = [];
+
+  fetch(url, {
+    headers: myHeaders,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let data1 = data.data;
+      console.log(data1);
+      nextPageData = data1;
+    })
+    .catch((error) => {
+      console.error('An error occurred while loading store data:', error);
+    });
+  renderProdData(nextPageData);
+}
+
+function ProdInfinityScroll() {
+  const container = document.querySelector('.container');
+
+  container.addEventListener('scroll', () => {
+    const scrollHeight = container.scrollHeight;
+    const scrollTop = container.scrollTop;
+    const clientHeight = container.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      console.log("무한스크롤 실행");
+      loadMoreItems();
+    }
+  });
 }
 
 function renderSubCategoryBtn() {
   const categoryDetails = document.getElementById("category-details");
-  if (categoryId == 1){
+  if (categoryId == 1) {
     categoryDetails.innerHTML = `
       <button class="fruits" type="button"><span>과일</span></button>
       <button class="vegetables" type="button"><span>채소</span></button>
@@ -53,14 +88,14 @@ function renderSubCategoryBtn() {
       <button class="mushrooms" type="button"><span>김/해조류</span></button>
       <button class="grains" type="button"><span>해산물/어패류</span></button>
       <button class="driedProduce" type="button"><span>수산가공물</span></button>
-    `;    
-  } 
+    `;
+  }
   else if (categoryId == 4) {
     categoryDetails.innerHTML = `
       <button class="fruits" type="button"><span>양념류</span></button>
       <button class="vegetables" type="button"><span>반찬류</span></button>
       <button class="mushrooms" type="button"><span>유제품</span></button>
-    `;        
+    `;
   }
   else {
     categoryDetails.innerHTML = `
@@ -69,16 +104,16 @@ function renderSubCategoryBtn() {
     <button class="mushrooms" type="button"><span>버섯</span></button>
     <button class="grains" type="button"><span>곡물</span></button>
     <button class="driedProduce" type="button"><span>건농산물</span></button>
-  `;        
+  `;
   }
 }
 
 function renderProdData(data) {
-    const productList = document.getElementById("product-list");
-    productsData.forEach(product => {
-        const li = document.createElement("li");
+  const productList = document.getElementById("product-list");
+  productsData.forEach(product => {
+    const li = document.createElement("li");
 
-        li.innerHTML = `
+    li.innerHTML = `
           <img src="${product.imageSrc}" alt="${product.productName}" class="prod-img">
           <div class="prod-info">
             <div class="product">
@@ -94,21 +129,19 @@ function renderProdData(data) {
           </div>
         `;
 
-        // li click 시 상세페이지로 이동
-        li.addEventListener("click", () => {
-            window.location.href = "../html/product-info.html";
-        })
-
-        productList.appendChild(li);
+    // li click 시 상세페이지로 이동
+    li.addEventListener("click", () => {
+      const productId = product.id;
+      window.location.href = `../html/product-info.html?id=${productId}`;
     })
+
+    productList.appendChild(li);
+  })
 }
 
-// function categoryFilter() {
-//     $('.category-details button').on('click', function (event) {
-//         event.preventDefault();
-//     });
-// }
-
 window.onload = function main() {
+  loadProdData();
+  renderProdData(fetchData);
   renderSubCategoryBtn();
+  ProdInfinityScroll();
 }
--- js/product-manage.js
+++ js/product-manage.js
@@ -1,22 +1,67 @@
+import { checkTokenValid, checkTokenExistence, checkUserRole } from './common/jwt_token_check.js';
 let fetchData = [];
-var myHeaders = new Headers;
+let pageNum = 0;
+let urls = `products?page=${pageNum}`;
 
-
+/* Header 설정 */
+const token = localStorage.getItem('access_token');
+var myHeaders = new Headers();
+myHeaders.append('Authorization', 'Bearer ' + token);
+myHeaders.append('Content-Type', 'application/json');
 
 function loadProdManageData() {
     /* 통신용 코드 */
-    fetch(url, {
-        // headers: myHeaders,
+    if (checkTokenExistence()) {
+        if (checkUserRole() === 'seller') {
+            fetch(urls, {
+                headers: myHeaders,
+                method: 'GET'
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    let data1 = data.data;
+                    fetchData = data1;
+                })
+                .catch((error) => {
+                    console.error('An error occurred while loading store data:', error);
+                });
+        }
+    }
+}
+
+function loadMoreItems() {
+    pageNum++;
+    let nextPageData = [];
+
+    fetch(urls, {
+        headers: myHeaders,
     })
         .then((response) => response.json())
         .then((data) => {
             let data1 = data.data;
-            console.log(data1);
-            fetchData = data1;
+            nextPageData = data1;
         })
         .catch((error) => {
             console.error('An error occurred while loading store data:', error);
         });
+    renderProdManageData(nextPageData);
+}
+
+function ProdInfinityScroll() {
+    const orderList = document.querySelector('.order-list');
+    console.log("함수실행");
+
+    container.addEventListener('scroll', () => {
+        console.log("scroll 리스너 실행");
+        const scrollHeight = orderList.scrollHeight;
+        const scrollTop = orderList.scrollTop;
+        const clientHeight = orderList.clientHeight;
+
+        if (scrollHeight-scrollTop >= (clientHeight+20)) {
+            console.log("무한스크롤실행");
+            loadMoreItems();
+        }
+    });
 }
 
 let indexOfClickBtn = 0;
@@ -28,74 +73,72 @@ function renderProdManageData(data) {
         const listItem = document.createElement('li');
         listItem.className = 'order-comp';
         listItem.id = item.id;
-    
+
         const image = document.createElement('img');
         image.src = item.imageUrl;
         image.alt = '';
-    
+
         const orderInfoBox = document.createElement('div');
         orderInfoBox.className = 'order-info-box';
-    
+
         const orderInfo = document.createElement('div');
         orderInfo.className = 'order-info';
-    
+
         const prodName = document.createElement('p');
         prodName.className = 'prod-name';
         prodName.textContent = item.productName;
-    
+
         const price = document.createElement('p');
         price.className = 'price';
         price.textContent = item.price;
-    
+
         const inventoryQuantity = document.createElement('p');
         inventoryQuantity.className = 'inventory-quantity';
         inventoryQuantity.textContent = '수량 ' + item.quantity;
-    
+
         orderInfo.appendChild(prodName);
         orderInfo.appendChild(price);
         orderInfo.appendChild(inventoryQuantity);
-    
+
         const btnBox = document.createElement('div');
         btnBox.className = 'btn-box';
-    
+
         const changeBtn = document.createElement('button');
         changeBtn.className = 'change-btn';
         changeBtn.textContent = '수정하기';
-        changeBtn.onclick = moveChangePage; // 버튼에 이벤트 연결
-    
+        changeBtn.addEventListener('click', () => {
+            const clickedItemId = item.id; // 클릭된 버튼의 항목 ID 가져오기
+            window.location.href = `../html/mypage_sell_enrollItem.html?id=${clickedItemId}`
+        });
+
         const deleteBtn = document.createElement('button');
         deleteBtn.className = 'delete-btn';
         deleteBtn.textContent = '삭제하기';
         deleteBtn.addEventListener('click', () => {
             const clickedItemId = item.id; // 클릭된 버튼의 항목 ID 가져오기
-            deleteOrderItem(clickedItemId);
+            deleteProduct(clickedItemId);
+            listItem.remove();
         });
-    
+
         btnBox.appendChild(changeBtn);
         btnBox.appendChild(deleteBtn);
-    
+
         orderInfoBox.appendChild(orderInfo);
         orderInfoBox.appendChild(btnBox);
-    
+
         listItem.appendChild(image);
         listItem.appendChild(orderInfoBox);
-    
+
         orderList.appendChild(listItem);
-    });    
+    });
 }
 
-function moveChangePage(){
-    // product id 같이 넘겨주기
-
-    window.location.href = "../html/mypage_sell_enrollItem.html";
-}
-
-function deleteProduct(itemId){
+function deleteProduct(itemId) {
     let url = `/products/${itemId}`
     /* 통신용 코드 */
     fetch(url, {
-        // method: 'delete',
-        // headers: myHeaders,
+        method: 'delete',
+        headers: myHeaders,
     })
         .then((response) => {
             response.json()
@@ -106,5 +149,14 @@ function deleteProduct(itemId){
         })
         .catch((error) => {
             console.error('An error occurred while loading store data:', error);
-        });    
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    ProdInfinityScroll();
+});
+
+window.onload = function main() {
+    loadProdManageData();
+    renderProdManageData(fetchData);
 }
